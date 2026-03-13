10bef7b7118a81f170ac99a7debe828fd6980e7d
Sakshee Zala <sakshee.zala@simformsolutions.com>
2026-03-13 17:11:57 +0530

    Initial commit - trainee frontend and backend

---
+++ trainee_backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+    res.json({ message: "Hello from the backend server!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend running on http://localhost:${PORT}`);
+});
